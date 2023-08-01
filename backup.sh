43831c79e4504e6610ccf694485239b45be9c372
xfy <135300152+xfy777@users.noreply.github.com>
2023-08-01 03:55:44 -0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -6,7 +6,7 @@ read action
 
 if [ "$action" = "push" ]; then
     rsync -av --exclude ".*" -e ssh /home/ sysop@10.0.2.2::NetBackup/Arch --password-file ~/rsync.password
-elif [ "$action" = "pull" ]; then
+    elif [ "$action" = "pull" ]; then
     rsync -av --exclude ".*" -e ssh sysop@10.0.2.2::NetBackup/Arch/ /home/ --password-file ~/rsync.password
 else
     echo "Invalid choice. Please enter either 'push' or 'pull'."
